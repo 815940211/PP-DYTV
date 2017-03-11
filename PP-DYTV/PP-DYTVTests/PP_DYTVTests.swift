3f12617b9c4ab76db7e59c87a7e9d0e6938702ff
黄平 <12345@qq.com>
2017-03-11 10:53:31 +0800

    项目初始化

---
+++ PP-DYTV/PP-DYTVTests/PP_DYTVTests.swift
@@ -0,0 +1,36 @@
+//
+//  PP_DYTVTests.swift
+//  PP-DYTVTests
+//
+//  Created by 黄平 on 2017/3/11.
+//  Copyright © 2017年 黄平. All rights reserved.
+//
+
+import XCTest
+@testable import PP_DYTV
+
+class PP_DYTVTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
